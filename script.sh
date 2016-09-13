6b4310b58205da1fc8ebca538d0ccd51d0a41f5e
Gunnar Andersson <gand@acm.org>
2016-09-13 17:27:59 +0200

    Quickfix: chmod executables

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+HOMEDIR=/home/vagrant
+
 apt-get update
 
 apt-get install -y lxde
@@ -7,4 +9,11 @@ apt-get install -y openjdk-7-jre
 apt-get install -y git wget unzip
 apt-get install -y d-feet
 apt-get install libegl1-mesa
-true
+
+# There's some bug currently with x flags disappearing from the filesystem
+# somewhere along the build pipeline.
+# Quick fix...
+chmod 755 $HOMEDIR/Qt-5.6/Tools/QtCreator/bin/qtcreator
+chmod 755 $HOMEDIR/gdp-sdk/eclipse/eclipse
+chmod 755 $HOMEDIR/dlt-viewer/dlt_viewer
+
