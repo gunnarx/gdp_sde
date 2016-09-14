032a63eb524a03ab91c25b23acf416eb7a2891c2
Gunnar Andersson <gand@acm.org>
2016-09-14 18:36:58 +0200

    $GO_QT_LOCATION Use temp staging dir for Qt in Go task

--- homedir/install-dlt-viewer.sh
+++ homedir/install-dlt-viewer.sh
@@ -2,17 +2,28 @@
 
 # Define QT_LOCATION in environment, or on command line
 if [ -n "$1" ] ; then
-  QT_LOCATION="$1"
+   QT_LOCATION="$1"
 fi
 
 if [ -z "$QT_LOCATION" ] ; then
-  echo "Please define QT_LOCATION variable, or give it as first argument."
-  exit 1
+   echo "Please define QT_LOCATION variable, or give it as first argument."
+   exit 1
+fi
+
+# This is a bit of a mess, but during the go build
+# the location is not the same as the final location...
+# so in that case install libs and plugins there, and create 
+# qt conf based on the final location.
+if [ -n "$GO_QT_LOCATION" ] ; then
+   INSTALL_LOCATION="$GO_QT_LOCATION"
+else
+   # If run on a normal system
+   INSTALL_LOCATION="$QT_LOCATION"
 fi
 
 echo "Installing libs and plugins into $QT_LOCATION"
-cp lib* $QT_LOCATION/lib/
-cp plugins/* $QT_LOCATION/plugins/
+cp lib* $INSTALL_LOCATION/lib/
+cp plugins/* $INSTALL_LOCATION/plugins/
 
 echo "Creating qt.conf"
 cat <<EOT >qt.conf
