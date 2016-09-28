05660a9e12c524ceecd11afb535b2120f9312591
Gunnar Andersson <gand@acm.org>
2016-09-28 10:50:40 +0200

    Add clean script for VMs

---
+++ recordtest/clean.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# (C) 2016 Gunnar Andersson
+# License: MPLv2
+
+# $1:  A unique Name prefix for the VM in question (no wildcards)
+[ -z "$1" ] && { echo "Usage: $0 <vbox-name (prefix)>" ; exit 1 ; }
+PATTERN="$1"
+
+D=$(dirname "$0") ; cd "$D"
+D="$PWD"
+
+echo "Unregistering old VMs matching pattern: $PATTERN :"
+VBoxManage list vms | fgrep "$PATTERN"
+for id in $(VBoxManage list vms | fgrep "$PATTERN" | awk '{print $2}' | sed 's/[{}]//g') ; do
+    VBoxManage controlvm $id poweroff
+    VBoxManage unregistervm $id 
+done
+
