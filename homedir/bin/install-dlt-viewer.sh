b3955100b94cc1c4d1c88b389f6743c5ece5a053
Gunnar Andersson <gand@acm.org>
2016-10-16 15:01:07 -0700

    Revert incorrect dlt-fix (qt.conf seems needed still)
    This needs to be improved at some point.

--- homedir/bin/install-dlt-viewer.sh
+++ homedir/bin/install-dlt-viewer.sh
@@ -12,17 +12,35 @@ if [ -z "$QT_LOCATION" ] ; then
    exit 1
 fi
 
-cat <<EOT >dlt-viewer_wrapper.sh
-#!/bin/sh
+# This is a bit of a mess, but during the go build
+# the location is not the same as the final location...
+# so in that case install libs and plugins there, and create 
+# qt conf based on the final location.
+if [ -n "$GO_QT_LOCATION" ] ; then
+   INSTALL_LOCATION="$GO_QT_LOCATION"
+else
+   # If run on a normal system
+   INSTALL_LOCATION="$QT_LOCATION"
+fi
 
 # Ensure libraries are found
 export LD_LIBRARY_PATH=$QT_LOCATION/lib:$DLT_VIEWER_LOCATION
 
+echo "Creating qt.conf"
+cat <<EOT >qt.conf
+[Paths]
+Prefix = $QT_LOCATION
+EOT
+
+cat <<EOT >dlt-viewer_wrapper.sh
+#!/bin/sh
+export LD_LIBRARY_PATH=$QT_LOCATION/lib
 # Sometimes dlt-viewer starts up without the title bar visible, causing it to
-# be unmoveable.  Seting a location/geometry prevents this.
-$DLT_VIEWER_LOCATION/dlt_viewer -geometry 1400x800+20+100
+# be unmoveable.  Let's set a location that prevents that it happens.
+/home/vagrant/dlt-viewer/dlt_viewer -geometry 1400x800+20+100
 EOT
 
 chmod 755 dlt-viewer_wrapper.sh
 
 echo "Done"
+echo "Remember to run the program with $LD_LIBRARY_PATH = $QT_LOCATION/lib in environment."
