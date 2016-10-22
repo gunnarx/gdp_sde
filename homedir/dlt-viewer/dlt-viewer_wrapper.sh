aa25a36f1960f4fb1e0e673662545646f31e5f2c
Gunnar Andersson <gand@acm.org>
2016-10-21 22:23:42 -0700

    Install dlt-viewer scripts using homedir
    A previous method used a script to create some files on the fly
    using parameterized location of Qt and dlt-viewer.
    
    That was then removed, but that left a buggy setup.
    
    For now, just put back the necessary files as they are under homedir and
    insert the known absolute path (/home/vagrant/...)
    Remove the cleverness and keep it simple - If $HOME changes later, rewrite
    these absolute paths.

--- homedir/dlt-viewer/dlt-viewer_wrapper.sh
+++ homedir/dlt-viewer/dlt-viewer_wrapper.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=/home/user/Qt/5.6/gcc_64/lib
-/home/vagrant/dlt-viewer/dlt_viewer
+export LD_LIBRARY_PATH=/home/user/Qt-5.6/5.6/gcc_64/lib:/home/vagrant/dlt-viewer
+
+# Sometimes dlt-viewer starts up without the title bar visible, causing it to
+# be unmoveable.  Let's set a location that prevents that it happens.
+/home/vagrant/dlt-viewer/dlt_viewer -geometry 1400x800+20+100
